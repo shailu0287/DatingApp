611facf5d76e2006e9d4ceff76c2fc7d6ad67927
Shailendra Sharma <shailu0287@gmail.com>
2020-09-14 13:52:52 +0530

    keyvalut

--- DatingAppAPI/Azure/KeyVault.cs
+++ DatingAppAPI/Azure/KeyVault.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Identity.Client;
+
 namespace DatingAppAPI.Azure
 {
     public class KeyVault : IKeyVault
@@ -13,10 +16,31 @@ namespace DatingAppAPI.Azure
 
         public async Task<string> GetKeyValue(string keyName)
         {
-            var client = new SecretClient(vaultUri: new Uri(vaultAddress), credential: new DefaultAzureCredential());
+            try
+            {
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                        {
+                            Delay= TimeSpan.FromSeconds(2),
+                            MaxDelay = TimeSpan.FromSeconds(16),
+                            MaxRetries = 5,
+                            Mode = RetryMode.Exponential
+                         }
+                };
+                var client = new SecretClient(new Uri(vaultAddress), new DefaultAzureCredential(), options);
 
-            KeyVaultSecret secret =await client.GetSecretAsync(keyName);
-            return secret.Value;
+                KeyVaultSecret secret = await client.GetSecretAsync("key1");
+
+                string secretValue = secret.Value;
+                return secretValue;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
         }
     }
 }
